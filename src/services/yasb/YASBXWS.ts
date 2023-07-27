e12dbe82fdc13d6d6094c6e7e0d273008cf726d2
raith <raithos@users.noreply.github.com>
2023-07-26 21:04:58 -0400

    replace serialize function with built in from js file

--- src/services/yasb/YASBXWS.ts
+++ src/services/yasb/YASBXWS.ts
@@ -1,46 +1,11 @@
 import { basicCardData, canonicalize } from './lib';
-import { CardData, UpgradeById } from "../../models/YASBCardData";
+import { serialtoxws } from "../../models/YASBCardData";
 import { Pilot, Vendor, XWSSquadron } from "../../models/XWSSquadron";
 import { serializedToShips } from "./permalink"
 
-const cardData: CardData = <CardData>basicCardData()
-
 export async function serializedToXWS(faction: string, serialized: string, squadName: string, obstacles: string, originalPath: string): Promise<XWSSquadron> {
 
-    const pilots: Pilot[] = serializedToShips(serialized)
-
-    const vendor = <Vendor>{
-        builder: 'squad2xws',
-        builder_url: 'http://squad2xws.objectivecat.com/',
-        link: "https://yasb.app/?" + originalPath
-    }
-
-    const vendorMap: Map<string, Vendor> = new Map()
-    vendorMap.set("squad2xws", vendor)
-
-    const xwsSquadron = <XWSSquadron>{
-        faction: toXWSFaction(faction),
-        pilots: pilots,
-        description: undefined,
-        obstacles: undefined,
-        points: 0,
-        vendor: vendorMap,
-        version: '2.6.0'
-    }
-
-    if (squadName !== undefined && ['Unnamed Squadron', 'New Squadron'].indexOf(squadName) == -1) {
-        xwsSquadron.name = squadName
-    }
-
-    if (obstacles !== undefined) {
-        let obs = obstacles.split(',')
-        if (obs.length == 3)
-            xwsSquadron.obstacles = obs
-    }
-
-    for (let pilot of xwsSquadron.pilots)
-        if (pilot.points != undefined)
-            xwsSquadron.points = (xwsSquadron.points || 0) + pilot.points
+    const xwsSquadron = JSON.parse(serialtoxws(serialized));
 
     return xwsSquadron;
 }
